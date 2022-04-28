2e2633d5e2ed49dfaeadd42fd7c6d6896829e5dc
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-04-27 22:34:55 -0300

    login page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+import logo from '../../assets/logo-tree.png';
+import './signin.css';
+
+
 function SignIn() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('CLICOU');
+  }
+
     return (
-      <div >
-        <h1>PAGINA DE LOGIN</h1>
+      <div className='container-center'>
+        <div className='login'>
+          <div className='login-area'>
+            <img src={logo} alt='System logo'/>
+          </div>
+          <form onSubmit={handleSubmit}>
+              <h1>Login SMS</h1>
+              <input type='text' placeholder='email@email.com' value={email} onChange={ (e) => setEmail(e.target.value) }/>
+              <input type='password' placeholder='******' value={password} onChange={ (e) => setPassword(e.target.value) }/>
+              <button type='submit'>Access</button>
+          </form>
+        </div>
       </div>
     );
 }
