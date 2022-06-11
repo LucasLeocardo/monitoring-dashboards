7d9dc9485d9601c114e12bc35a4157a1fb72bed4
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-06-11 19:53:21 -0300

    first steps in historical data

--- src/components/ViewHistoricalData/ViewHistoricalData.js
+++ src/components/ViewHistoricalData/ViewHistoricalData.js
@@ -1,19 +1,138 @@
 import * as React from 'react';
 import './viewHistoricalData.css'
 import { AuthContext } from '../../contexts/auth';
+import { useNavigate } from 'react-router-dom';
+import * as ResponseStatus from '../../entities/responseStatus';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+import * as EndPoints from '../../entities/endPoints'
+import Loading from '../Loading/Loading';
+import TextField from '@mui/material/TextField';
+import { useParams } from "react-router-dom";
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    height: '500px',
+    color: theme.palette.text.secondary,
+  }));
 
 export default function ViewHistoricalData() {
 
+    const { deviceId } = useParams();
     const { API, setSelectedPage, user } = React.useContext(AuthContext); 
+    const navigate = useNavigate();
+    const [isDataAvailable, setIsDataAvailable] = React.useState(false);
+    const [deviceList, setDeviceList] =  React.useState([]);
+    const mounted = React.useRef();
+    const [startDate, setStartDate] = React.useState(new Date());
+    const [endDate, setEndDate] = React.useState(new Date());
 
     React.useEffect(() => {
-        setSelectedPage('');
+        if (!mounted.current) { 
+            startDate.setMonth(startDate.getMonth() - 1);
+            getActiveDevicesAsync();
+            setSelectedPage('');
+            mounted.current = true;
+        } 
     }, []);
+
+    const getActiveDevicesAsync = async () => {
+        await API(EndPoints.BASE_ENDPOINT, user.token).get(EndPoints.GET_ACTIVE_DEVICES)
+            .then( response => {
+                if (response.status === ResponseStatus.SUCCESS) {
+                    setDeviceList(response.data);
+                    setIsDataAvailable(true);
+                }
+            })
+            .catch(error => ( error ));
+    }
+
+    const onSelectedDeviceChange = (event, values) => {
+        if (values) {
+            navigate(`/view-historical-data/${values._id}`);
+        }
+    }
+
+    const getDefaultValue = () => {
+        const defaultDevice = deviceList.find(device => device._id === deviceId);
+        return defaultDevice
+    }
+
+    if (!isDataAvailable) {
+        return (
+            <Loading/>
+        );
+    }
 
     return (
         <div>
-            <h1>View Historical Data</h1>
+            <Stack spacing={1} sx={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Stack spacing={3} sx={{ width: '600px'}}>
+                    <Autocomplete
+                        options={deviceList}
+                        getOptionLabel={option => option.name}
+                        defaultValue={getDefaultValue}
+                        onChange={onSelectedDeviceChange}
+                        id="auto-complete"
+                        autoComplete
+                        includeInputInList
+                        renderInput={(params) => ( 
+                            <TextField 
+                                {...params}  
+                                label="Select a Device" 
+                                variant="standard" 
+                                color="success"
+                            /> 
+                        )}
+                    />
+                </Stack>
+                <Stack spacing={3} sx={{ width: '600px', display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DesktopDatePicker
+                            label="Start date"
+                            value={startDate}
+                            minDate={new Date('2022-01-01')}
+                            onChange={(newValue) => {
+                                setStartDate(newValue);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                        <DesktopDatePicker
+                            label="End date"
+                            value={endDate}
+                            minDate={new Date('2022-01-01')}
+                            onChange={(newValue) => {
+                                setEndDate(newValue);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
+                </Stack>
+            </Stack>
+            <Box sx={{ flexGrow: 1, marginTop: '40px', paddingBottom: '40px' }}>
+                <Grid container spacing={4}>
+                <Grid item xs={12}>
+                        <Item>
+                            HAHAHAAH
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
     );
 }
